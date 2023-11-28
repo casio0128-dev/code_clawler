06089e708bed9753adf59f2d9a43ad8a003a79fb
Sourai0443 <sourai0443@gmail.com>
2023-11-28 21:24:15 +0900

    testまでのPATHが表示されるようしã修正

--- main.go
+++ main.go
@@ -3,11 +3,12 @@ package main
 import (
 	"fmt"
 	"os"
+	"strings"
 	"sync"
 )
 
 var (
-	args
+	args []string
 )
 
 func init() {
@@ -16,18 +17,38 @@ func init() {
 
 func main() {
 	wg := &sync.WaitGroup{}
-	fmt.Println(args)
 	for _, arg := range args {
 		wg.Add(1)
-		go func() {
+		fp, err := os.Open(arg)
+		if err != nil {
+			panic(err)
+		}
+		go func(f *os.File, a string) {
 			defer wg.Done()
-			fp, err := os.OpenFile(arg, os.O_CREATE | os.O_RDWR, 0755)
+			cwd := getCWD(a)
+			fmt.Println(cwd)
+			fp, err := os.OpenFile(cwd, os.O_CREATE|os.O_RDWR, 0755)
 			if err != nil {
 				panic(err)
 			}
-			defer fp.Close()
-
-		}()
+			defer func() {
+				if err := fp.Close(); err != nil {
+					panic(err)
+				}
+			}()
+		}(fp, arg)
 	}
 	wg.Wait()
+}
+
+func getCWD(dirName string) string {
+	wd, err := os.Getwd()
+	if err != nil {
+		panic(err)
+	}
+	return JoinFilePath(wd, dirName)
+}
+
+func JoinFilePath(paths ...string) string {
+	return strings.Join(paths, string(os.PathSeparator))
 }
